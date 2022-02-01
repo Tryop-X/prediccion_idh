ad5198e21179a3697e815742d6072ce4125625b2
tryopx80P <tryopx80p@tryopx80p.mshome.net>
2022-02-01 07:35:16 -0500

    agregando el enviroment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    loadChildren: ()=>
-    import('./admin/admin.module').then((m)=>m.AdminModule)
-  },
 
   {
     path: '',
